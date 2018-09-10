0c12c2c9426f71fef590d3eb6488007eafe226d9
IsmaelCota <36893961+IsmaelCota@users.noreply.github.com>
2018-09-10 08:06:32 -0700

    Source Code For PikachuChallenge
    Souce Code

---
+++ PikachuChallenge.java
@@ -0,0 +1,10 @@
+public class PikachuChallenge {
+
+	public static void main(String[] args) {
+		System.out.println("Pikachu welcomes you to the world of Pokémon!");
+		System.out.println("(\\__/)");
+		System.out.println("o^.^");
+		System.out.println("z(_(\")(\")");
+	}
+
+}
